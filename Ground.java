8bd4a85db487befba8097356cedd81a20bae0611
Michael Rejo <32908690+MichaelRejo1601@users.noreply.github.com>
2019-09-19 09:36:18 -0700

    Added Images and Changed Dimensions

--- Ground.java
+++ Ground.java
@@ -17,7 +17,7 @@ public class Ground extends World
     public Ground()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1200, 600, 1); 
         prepare();
         score = 0;
         time = 5;
