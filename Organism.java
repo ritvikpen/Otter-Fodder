4c267eb656007c22af872638ec4121edaaf1b0ee
Michael Rejo <32908690+MichaelRejo1601@users.noreply.github.com>
2019-09-20 09:24:35 -0700

    Added Abstract Class

--- Organism.java
+++ Organism.java
@@ -6,14 +6,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Organism extends Actor
+abstract class Organism extends Actor
 {
     /**
      * Act - do whatever the Organism wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+    abstract double SpawnRate(); 
 }
