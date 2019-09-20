05080c9bb05551b1db88715b8374e4316d9c8993
Likhit Gudisay <likhit0226@gmail.com>
2019-09-19 19:08:50 -0700

    abstract

--- Organism.java
+++ Organism.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Banana here.
+ * Write a description of class Organism here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Banana extends Organism
+public class Organism extends Actor
 {
     /**
-     * Act - do whatever the Banana wants to do. This method is called whenever
+     * Act - do whatever the Organism wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- Spider1.java
+++ Spider1.java
@@ -2,36 +2,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Spider1 here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Spider1 extends Villain
+public class Spider1 extends Organism
 {
-    
+
     /**
      * Act - do whatever the Spider1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
         if ( isAtEdge() )
         {
             turn(30);
         }
-        
-        if ( Greenfoot.getRandomNumber(100) < 10 )
+
+        if ( Greenfoot.getRandomNumber(300) < 10 )
         {
             turn(Greenfoot.getRandomNumber(90)-45);
         }
-        
+
         move(1);
-        
+
         if (isTouching(Lizard.class) )
         {
             Greenfoot.playSound("Eating.wav");
             removeTouching(Lizard.class);
-        }    
-        
-    }    
+        }
+
+    }
 }
